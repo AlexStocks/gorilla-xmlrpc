a3becf3a45aa834f2512ef07f1fa561553c5600b
AlexStocks <alexstocks@foxmail.com>
2019-02-01 20:39:03 +0800

    Mod: format xml string

--- xml/xml2rpc_test.go
+++ xml/xml2rpc_test.go
@@ -6,6 +6,7 @@ package xml
 
 import (
 	"reflect"
+	"strings"
 	"testing"
 	"time"
 )
@@ -28,11 +29,86 @@ type StructXml2Rpc struct {
 
 func TestXML2RPC(t *testing.T) {
 	req := new(StructXml2Rpc)
-	err := xml2RPC("<methodCall><methodName>Some.Method</methodName><params><param><value><i4>123</i4></value></param><param><value><double>3.145926</double></value></param><param><value><string>Hello, World!</string></value></param><param><value><boolean>0</boolean></value></param><param><value><struct><member><name>Foo</name><value><int>42</int></value></member><member><name>Bar</name><value><string>I'm Bar</string></value></member><member><name>Data</name><value><array><data><value><int>1</int></value><value><int>2</int></value><value><int>3</int></value></data></array></value></member></struct></value></param><param><value><dateTime.iso8601>20120717T14:08:55</dateTime.iso8601></value></param><param><value><base64>eW91IGNhbid0IHJlYWQgdGhpcyE=</base64></value></param></params></methodCall>", req)
+	xmlStr := `
+<methodCall>
+	<methodName>Some.Method</methodName>
+	<params>
+			<param>
+					<value>
+							<i4>123</i4>
+					</value>
+			</param>
+			<param>
+					<value>
+							<double>3.145926</double>
+					</value>
+			</param>
+			<param>
+					<value>
+							<string>Hello, World!</string>
+					</value>
+			</param>
+			<param>
+					<value>
+							<boolean>0</boolean>
+					</value>
+			</param>
+			<param>
+					<value>
+							<struct>
+									<member>
+											<name>Foo</name>
+											<value>
+													<int>42</int>
+											</value>
+									</member>
+									<member>
+											<name>Bar</name>
+											<value>
+													<string>I'm Bar</string>
+											</value>
+									</member>
+									<member>
+											<name>Data</name>
+											<value>
+													<array>
+															<data>
+																	<value>
+																			<int>1</int>
+																	</value>
+																	<value>
+																			<int>2</int>
+																	</value>
+																	<value>
+																			<int>3</int>
+																	</value>
+															</data>
+													</array>
+											</value>
+									</member>
+							</struct>
+					</value>
+			</param>
+			<param>
+					<value>
+							<dateTime.iso8601>20120717T14:08:55</dateTime.iso8601>
+					</value>
+			</param>
+			<param>
+					<value>
+							<base64>eW91IGNhbid0IHJlYWQgdGhpcyE=</base64>
+					</value>
+			</param>
+	</params>
+</methodCall>`
+	err := xml2RPC(xmlStr, req)
 	if err != nil {
 		t.Error("XML2RPC conversion failed", err)
 	}
-	expected_req := &StructXml2Rpc{123, 3.145926, "Hello, World!", false, SubStructXml2Rpc{42, "I'm Bar", []int{1, 2, 3}}, time.Date(2012, time.July, 17, 14, 8, 55, 0, time.Local), []byte("you can't read this!")}
+	expected_req := &StructXml2Rpc{123, 3.145926, "Hello, World!",
+		false, SubStructXml2Rpc{42, "I'm Bar",
+			[]int{1, 2, 3}}, time.Date(2012, time.July, 17, 14, 8, 55, 0, time.Local),
+		[]byte("you can't read this!")}
 	if !reflect.DeepEqual(req, expected_req) {
 		t.Error("XML2RPC conversion failed")
 		t.Error("Expected", expected_req)
@@ -87,8 +163,38 @@ type StructXml2RpcHelloArgs struct {
 }
 
 func TestXML2RPCLowercasedMethods(t *testing.T) {
+	xmlStr := `
+    <methodCall>
+    	<params>
+    			<param>
+    					<value>
+    							<struct>
+    									<member>
+    											<name>string1</name>
+    											<value>
+    													<string>I'm a first string</string>
+    											</value>
+    									</member>
+    									<member>
+    											<name>string2</name>
+    											<value>
+    													<string>I'm a second string</string>
+    											</value>
+    									</member>
+    									<member>
+    											<name>id</name>
+    											<value>
+    													<int>1</int>
+    											</value>
+    									</member>
+    							</struct>
+    					</value>
+    			</param>
+    	</params>
+    </methodCall>
+	`
 	req := new(StructXml2RpcHelloArgs)
-	err := xml2RPC("<methodCall><params><param><value><struct><member><name>string1</name><value><string>I'm a first string</string></value></member><member><name>string2</name><value><string>I'm a second string</string></value></member><member><name>id</name><value><int>1</int></value></member></struct></value></param></params></methodCall>", req)
+	err := xml2RPC(xmlStr, req)
 	if err != nil {
 		t.Error("XML2RPC conversion failed", err)
 	}
@@ -103,10 +209,11 @@ func TestXML2RPCLowercasedMethods(t *testing.T) {
 
 func TestXML2PRCFaultCall(t *testing.T) {
 	req := new(StructXml2RpcHelloArgs)
-	data := `<?xmlversion="1.0"?><methodResponse><fault><value><struct><member><name>faultCode</name><value><int>116</int></value></member><member><name>faultString</name><value><string>Error
+	data := `<?xmlversion="1.0"?><methodResponse><fault><value><struct><member><name>faultCode</name>
+	  <value><int>116</int></value></member><member><name>faultString</name><value><string>Error
 Requiredattribute'user'notfound:
 [{'User',"gggg"},{'Host',"sss.com"},{'Password',"ssddfsdf"}]
-</string></value></member></struct></value></fault></methodResponse>`
+    </string></value></member></struct></value></fault></methodResponse>`
 
 	errstr := `Error
 Requiredattribute'user'notfound:
@@ -122,7 +229,7 @@ Requiredattribute'user'notfound:
 		if fault.Code != 116 {
 			t.Errorf("expected fault.Code to be %d, but got %d", 116, fault.Code)
 		}
-		if fault.String != errstr {
+		if strings.TrimSpace(fault.String) != strings.TrimSpace(errstr) {
 			t.Errorf("fault.String should be:\n\n%s\n\nbut got:\n\n%s\n", errstr, fault.String)
 		}
 	}
@@ -130,7 +237,8 @@ Requiredattribute'user'notfound:
 
 func TestXML2PRCISO88591(t *testing.T) {
 	req := new(StructXml2RpcHelloArgs)
-	data := `<?xml version="1.0" encoding="ISO-8859-1"?><methodResponse><fault><value><struct><member><name>faultCode</name><value><int>116</int></value></member><member><name>faultString</name><value><string>Error
+	data := `<?xml version="1.0" encoding="ISO-8859-1"?><methodResponse><fault><value><struct><member><name>faultCode</name>
+	<value><int>116</int></value></member><member><name>faultString</name><value><string>Error
 Requiredattribute'user'notfound:
 [{'User',"` + "\xd6\xf1\xe4" + `"},{'Host',"sss.com"},{'Password',"ssddfsdf"}]
 </string></value></member></struct></value></fault></methodResponse>`
@@ -154,3 +262,9 @@ Requiredattribute'user'notfound:
 		}
 	}
 }
+
+// func TestSupervisorMulticall(t *testing.T) {
+// 	// data := `<methodCall><methodName>system.multicall</methodName><params><param><value><array><data><value><struct><member><name>methodName</name><value><string>supervisor.getSupervisorVersion</string></value></member><member><name>params</name><value><array><data></data></array></value></member></struct></value><value><struct><member><name>methodName</name><value><string>supervisor.getAPIVersion</string></value></member><member><name>params</name><value><array><data></data></array></value></member></struct></value><value><struct><member><name>methodName</name><value><string>supervisor.getState</string></value></member><member><name>params</name><value><array><data></data></array></value></member></struct></value><value><struct><member><name>methodName</name><value><string>supervisor.getPID</string></value></member><member><name>params</name><value><array><data></data></array></value></member></struct></value></data></array></value></param></params></methodCall>`
+// }
+
+// `<methodResponse><params><param><value><array><data><value><string>3.1.4</string></value><value><string>3.0</string></value><value><struct><member><name>statename</name><value><string>RUNNING</string></value></member><member><name>statecode</name><value><int>1</int></value></member></struct></value><value><int>15146</int></value></data></array></value></param></params></methodResponse>`
